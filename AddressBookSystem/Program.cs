be78a5c88285d496d85aa88486cd8d9ab39e8790
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-14 06:43:19 +0530

    [Shivani] Insert new Contact in address book

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book System");
+            ContactModel contact = new ContactModel();
+            InsertContact insert = new InsertContact();
+            contact.FirstName = "Muskan";
+            contact.LastName = "Pathak";
+            contact.Address = "Ujjain";
+            contact.City = "Ujjain";
+            contact.State = "Madhya Pradesh";
+            contact.Zip = 456601;
+            contact.PhoneNumber = 7755869423;
+            contact.Email = "muskan@gmail.com";
+
+            insert.AddContact(contact);
+        }
+    }
+}
